eca4d5ae82d1176b2e9bd106b35b372fa8008e0c
Lorenzo Fontoura <lorenzo@nioel.com>
2020-07-09 02:09:36 +1000

    Add Meshing class

---
+++ Runtime/Meshing.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Mathematics;
+using Unity.Physics;
+using UnityEngine;
+
+namespace DOTools {
+
+    public static class Meshing {
+
+        public static void ScaleMesh(ref Mesh mesh, float3 scale) {
+            Vector3[] vertices = mesh.vertices;
+
+            for (int i = 0; i < vertices.Length; i++) {
+                vertices[i].x *= scale.x;
+                vertices[i].y *= scale.y;
+                vertices[i].z *= scale.z;
+            }
+
+            mesh.vertices = vertices;
+            mesh.RecalculateBounds();
+        }
+
+    }
+
+}
