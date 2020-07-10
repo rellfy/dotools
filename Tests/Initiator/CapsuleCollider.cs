89bfb4606bd6b5e9c22449c6f5a55f0641926003
Lorenzo Fontoura <lorenzo@nioel.com>
2020-07-10 16:07:51 +1000

    Add Capsuleollider initiator tests

---
+++ Tests/Initiator/CapsuleCollider.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Transforms;
+
+namespace DOTools.Tests.Initiator {
+
+    public class CapsuleCollider {
+
+        private const string WorldName = "TestWorld";
+
+        private (World, Entity) CreateCapsuleCollider(
+            CapsuleGeometry geometry = default,
+            CollisionFilter filter = default,
+            Material material = default
+        ) {
+            World world = new World(WorldName);
+            Entity entity = DOTools.Initiator
+                .Transform(world)
+                .InitiateCapsuleCollider(
+                    world,
+                    geometry,
+                    filter,
+                    material
+                );
+
+            return (world, entity);
+        }
+
+        [Test]
+        public void CreateCapsuleCollider_DoesNotThrow() {
+            (World world, Entity entity) = CreateCapsuleCollider();
+
+            Assert.DoesNotThrow(() => {
+                world.EntityManager.GetComponentData<PhysicsCollider>(entity);
+            });
+        }
+
+    }
+
+}
