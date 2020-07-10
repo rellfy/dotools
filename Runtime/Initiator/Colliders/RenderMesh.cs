b37906e8abefd9b3ca6daa6f9573600a26ea4a10
Lorenzo Fontoura <lorenzo@nioel.com>
2020-07-10 16:42:34 +1000

    Add RenderMesh initiator and tests

---
+++ Runtime/Initiator/Colliders/RenderMesh.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Physics;
+using Unity.Rendering;
+using Unity.Transforms;
+using UnityEngine;
+using UnityEngine.Rendering;
+using Material = UnityEngine.Material;
+
+namespace DOTools {
+
+    public static partial class Initiator {
+
+        private static readonly RenderMesh DefaultRenderMesh = new RenderMesh() {
+            castShadows = ShadowCastingMode.On,
+            receiveShadows = true,
+            material = default,
+            mesh = default
+        };
+
+        private static Entity GetEntityWithRenderMeshRequirements(World world) {
+            Entity entity = Transform(world);
+
+            world.EntityManager.AddComponents(entity,
+                new ComponentTypes(
+                    ComponentType.ReadWrite<RenderBounds>(),
+                    ComponentType.ReadWrite<WorldRenderBounds>(),
+                    ComponentType.ReadWrite<ChunkWorldRenderBounds>()
+                )
+            );
+
+            return entity;
+        }
+
+        private static Entity RenderMesh(
+            Entity entity,
+            World world,
+            RenderMesh renderMesh
+        ) {
+            try {
+                world
+                    .EntityManager
+                    .GetComponentData<LocalToWorld>(entity);
+            } catch (ArgumentException) {
+                entity.InitiateTransform(world);
+            }
+
+            world.EntityManager.AddSharedComponentData(entity, renderMesh);
+
+            return entity;
+        }
+
+        public static Entity InitiateRenderMesh(
+            this Entity entity,
+            World world,
+            RenderMesh renderMesh
+        ) {
+            return RenderMesh(
+                entity,
+                world,
+                renderMesh
+            );
+        }
+
+        public static Entity InitiateRenderMesh(
+            this Entity entity,
+            World world,
+            Mesh mesh,
+            Material material
+        ) {
+            RenderMesh renderMesh = DefaultRenderMesh;
+            renderMesh.mesh = mesh;
+            renderMesh.material = material;
+
+            return RenderMesh(
+                entity,
+                world,
+                renderMesh
+            );
+        }
+
+        public static Entity RenderMesh(
+            World world,
+            RenderMesh renderMesh
+        ) {
+            Entity entity = GetEntityWithRenderMeshRequirements(world);
+
+            return RenderMesh(
+                entity,
+                world,
+                renderMesh
+            );
+        }
+
+        public static Entity RenderMesh(
+            World world,
+            Mesh mesh,
+            Material material
+        ) {
+            RenderMesh renderMesh = DefaultRenderMesh;
+            renderMesh.mesh = mesh;
+            renderMesh.material = material;
+
+            return RenderMesh(
+                world,
+                renderMesh
+            );
+        }
+
+    }
+
+}
---
+++ Tests/Initiator/RenderMesh.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Unity.Entities;
+using Unity.Rendering;
+using UnityEngine;
+using Material = UnityEngine.Material;
+
+namespace DOTools.Tests.Initiator {
+
+    public class RenderMesh {
+
+        private const string WorldName = "TestWorld";
+
+        private (World, Entity) CreateRenderMesh(
+            Mesh mesh = default,
+            Material material = default
+        ) {
+            World world = new World(WorldName);
+            Entity entity = DOTools.Initiator
+                .Transform(world)
+                .InitiateBoxCollider(world)
+                .InitiateRenderMesh(world, mesh, material);
+
+            return (world, entity);
+        }
+
+        [Test]
+        public void CreateRenderMesh_DoesNotThrow() {
+            (World world, Entity entity) = CreateRenderMesh();
+
+            Assert.DoesNotThrow(() => {
+                world.EntityManager.GetSharedComponentData<Unity.Rendering.RenderMesh>(entity);
+            });
+        }
+
+    }
+
+}
