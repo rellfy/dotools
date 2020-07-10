657e33f73bc758ad2e0ac1a3d4a3f175880f26d1
Lorenzo Fontoura <lorenzo@nioel.com>
2020-07-10 16:09:45 +1000

    Add Collider initiator (shared)

---
+++ Runtime/Initiator/Colliders/Collider.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Entities;
+using Unity.Physics;
+using Material = Unity.Physics.Material;
+
+namespace DOTools {
+
+    public static partial class Initiator {
+
+        private static readonly CollisionFilter DefaultFilter = default;
+        private static readonly Material DefaultMaterial = default;
+
+        private static Entity GetEntityWithColliderRequirements(World world) {
+            Entity entity = Transform(world);
+
+            world.EntityManager.AddComponent(entity,
+                ComponentType.ReadWrite<PhysicsCollider>()
+            );
+
+            return entity;
+        }
+
+    }
+
+}
