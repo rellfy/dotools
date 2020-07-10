592516175e0ddcc2571dc1225cd08c139028890e
Lorenzo Fontoura <lorenzo@nioel.com>
2020-07-10 17:24:24 +1000

    Add sample

---
+++ Samples/RenderCube.cs
@@ -0,0 +1,29 @@
+﻿#if DOTOOLS_SAMPLES
+using DOTools;
+using Unity.Entities;
+using UnityEngine;
+
+public class RenderCubeSample : SystemBase {
+
+    protected override void OnCreate() {
+        (Mesh mesh, Material material) = GetDefaultMeshAndMaterial();
+
+        DOTools.Initiator
+            .Transform(World)
+            .InitiateBoxCollider(World)
+            .InitiateRenderMesh(World, mesh, material);
+    }
+
+    protected override void OnUpdate() { }
+
+    private (Mesh, Material) GetDefaultMeshAndMaterial() {
+        GameObject temp = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        Mesh mesh = temp.GetComponent<MeshFilter>().sharedMesh;
+        Material material = temp.GetComponent<MeshRenderer>().sharedMaterial;
+        Object.Destroy(temp);
+
+        return (mesh, material);
+    }
+
+}
+#endif
